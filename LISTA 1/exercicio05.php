c6869d527dc59688e37a39a52ad4d2a649349168
PaulaAnacleto <paulaanacleto15@gmail.com>
2025-06-12 12:12:18 -0300

    correção do exercício 5

--- LISTA 1/exercicio05.php
+++ LISTA 1/exercicio05.php
@@ -3,8 +3,8 @@
     $vitoria = 1;
     $derrota = 1;
 
-    $pontos_final += $pontos + ($vitorias * 20);
-    $pontos_final -= $pontos - ($derrotas * 15);
+    $pontos_final += $pontos + ($vitoria * 20);
+    $pontos_final -= $pontos - ($derrota * 15);
 
 
     echo "Pontuação inicial: $pontos pontos<br>";
